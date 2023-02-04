dcf82809c38cc609dc552be6f6548b35e4e2bb0c
shashankshri0802 <124416389+shashankshri0802@users.noreply.github.com>
2023-02-04 09:29:14 +0530

    Update sales_dash_v1.py

--- sales_dash_v1.py
+++ sales_dash_v1.py
@@ -2,10 +2,12 @@ import streamlit as st
 import pandas as pd
 
 df = pd.read_csv('data_sheet.csv')
+branch_list = df.BRANCH_CODE.sort_values(ascending=Ture).unique()
 
 col1, col2, col3, col4, col5 = st.columns(5)
 
 date1 = col1.selectbox('Start Date',options = [1,2,...,365])
 date2 = col2.selectbox('End Date', options = [1,2,...,365])
+branch = col3.selectbox('Select Branch',options = branch_list)
 
 st.dataframe(df)
