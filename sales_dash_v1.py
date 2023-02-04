9e12321ecbd0db76fe231c6a4a249efcb0c45856
shashankshri0802 <124416389+shashankshri0802@users.noreply.github.com>
2023-02-04 09:30:53 +0530

    Update sales_dash_v1.py

--- sales_dash_v1.py
+++ sales_dash_v1.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 df = pd.read_csv('data_sheet.csv')
 branch_list = df.BRANCH_CODE.unique().tolist()
-branch_list.insert(0,0)
+branch_list.insert(0,'ALL')
 
 col1, col2, col3, col4, col5 = st.columns(5)
 
