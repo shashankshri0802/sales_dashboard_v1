dbd82c31117d24c620d156dbe6c4aac9c4a0de3a
shashankshri0802 <124416389+shashankshri0802@users.noreply.github.com>
2023-02-04 09:30:35 +0530

    Update sales_dash_v1.py

--- sales_dash_v1.py
+++ sales_dash_v1.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 
 df = pd.read_csv('data_sheet.csv')
-branch_list = df.BRANCH_CODE.unique()
+branch_list = df.BRANCH_CODE.unique().tolist()
+branch_list.insert(0,0)
 
 col1, col2, col3, col4, col5 = st.columns(5)
 
