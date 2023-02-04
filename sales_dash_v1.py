d8d0a1afe7ed816c01393f169f499ff9ec179998
shashankshri0802 <124416389+shashankshri0802@users.noreply.github.com>
2023-02-04 09:26:17 +0530

    Update sales_dash_v1.py

--- sales_dash_v1.py
+++ sales_dash_v1.py
@@ -3,4 +3,7 @@ import pandas as pd
 
 df = pd.read_csv('data_sheet.csv')
 
+date1 = st.selectbox('Start Date',options = [1,2,...,365])
+date2 = st.selectbox('End Date', options = [1,2,...,365])
+
 st.dataframe(df)
